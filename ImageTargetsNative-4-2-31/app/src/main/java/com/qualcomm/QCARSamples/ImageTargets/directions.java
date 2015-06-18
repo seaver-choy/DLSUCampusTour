98a53bf67aa4c2feecab21071f9daf5041cc77d2
PillowSoPaw <amadorable.angelo@gmail.com>
2015-06-18 10:46:06 +0800

    Revert "adjusted main menu buttons"
    This reverts commit 03f0f2684d2bf7e606d85ef505e5c0c5509ba4c8.

--- ImageTargetsNative-4-2-31/app/src/main/java/com/qualcomm/QCARSamples/ImageTargets/directions.java
+++ ImageTargetsNative-4-2-31/app/src/main/java/com/qualcomm/QCARSamples/ImageTargets/directions.java
@@ -41,7 +41,7 @@ public class directions extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_directions, menu);
+        getMenuInflater().inflate(R.menu.menu_directions, menu);
         return true;
     }
 
